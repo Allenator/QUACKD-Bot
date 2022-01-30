5d88c092a134fc4a68ce31bccac7e5ccf361ec21
Allen Mi <allen.mi@yale.edu>
2022-01-30 11:54:40 -0500

    Prepare for final submission

--- run_app.sh
+++ run_app.sh
@@ -6,6 +6,6 @@ QI_AUTH_PATH="credentials/qi.json"
 
 # Specify backend for B92 protocol
 # Available options are "aer", "qi_sim", "qi_starmon"
-BACKEND="qi_sim"
+BACKEND="qi_starmon"
 
 ${PYTHON_PATH} quackd/app.py ${SLACK_TOKENS_PATH} ${BACKEND} --qi_auth_path ${QI_AUTH_PATH}
