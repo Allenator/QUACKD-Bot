5f00cb77f59019d671546d3bc79a1e14300ea893
Allen Mi <allen.mi@yale.edu>
2022-01-30 20:56:32 -0500

    Run script now activates conda env

--- run_app.sh
+++ run_app.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+CONDA_PATH="conda"
+CONDA_ENV="quackd"
 PYTHON_PATH="python3"
+
 SLACK_TOKENS_PATH="credentials/slack.json"
 QI_AUTH_PATH="credentials/qi.json"
 KEYCHAIN_PATH="data/keychain.json"
@@ -8,6 +11,9 @@ KEYCHAIN_PATH="data/keychain.json"
 # Specify backend for B92 protocol
 # Available options are "aer", "qi_sim", "qi_starmon"
 BACKEND="aer"
+
+eval "$(${CONDA_PATH} shell.bash hook)"
+${CONDA_PATH} activate ${CONDA_ENV}
 
 ${PYTHON_PATH} quackd/app.py \
     ${SLACK_TOKENS_PATH} ${BACKEND} \
